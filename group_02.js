4b04d176cebc644c1f500516739f0f7057cbda82
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-10-27 10:36:15 -0500

    Added a switch with group_2.js file

---
+++ group_02.js
@@ -0,0 +1,47 @@
+var atticus = ["Atticus", "2405", "47000", 3];
+var jem = ["Jem", "62347", "63500", 4];
+var boo = ["Boo", "11435", "54000", 3];
+var scout = ["Scout", "6243", "74750", 5];
+var robert = ["Robert", "26835", "66000", 1];
+var mayella = ["Mayella", "89068", "35000", 2];
+
+var employees = [atticus, jem, boo, scout, robert, mayella];
+
+function bonus(employee) {
+  var bonusArray = [];
+  bonusArray[0] = employee[0];
+
+  switch (employee[3]) {
+    case 1: bonusArray[1] = 0;
+    case 2: bonusArray[1] = 0;
+    break;
+    case 3: bonusArray[1] = 0.04;
+    break;
+    case 4: bonusArray[1] = 0.06;
+    break;
+    case 5: bonusArray[1] = 0.10;
+    break;
+};
+    if (employee[1].length == 4) {
+      bonusArray[1] += .05;
+    }
+    if (parseInt(employee[2]) > 65000) {
+      bonusArray[1] -= .01;
+    }
+    if (bonusArray[1] > .13) {
+        bonusArray[1] = .13;
+    } else if (bonusArray[1] < 0) {
+        bonusArray[1] = 0;
+    }
+
+    bonusArray[2] = parseInt(employee[2]) * (1 + bonusArray[1]);
+
+    bonusArray[3] = Math.round(bonusArray[2]-employee[2]);
+
+    return bonusArray;
+
+  }
+
+  for (var i = 0; i < employees.length; i++) {
+    console.log(bonus(employees[i]));
+  }
